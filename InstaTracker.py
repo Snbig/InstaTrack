d0002ff330206515b2503f98f29b2158818dbb68
Hamed <Snbig@users.noreply.github.com>
2019-06-28 15:02:57 +0430

    Bug Fixed.
    Excepions on invalid or diactivated users and [Instagram Graph API Rate limiting](https://developers.facebook.com/docs/instagram-api/overview/#rate-limiting) cached.

--- InstaTracker.py
+++ InstaTracker.py
@@ -10,41 +10,66 @@ import re
 
 import requests
 
+authtokens = tuple()
+
+
+def checkTokens():
+    if not authtokens:
+        getTokens()
+
+
+def getTokens():
+    r = requests.get('https://instagram.com/instagram/', headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0', }).text
+    rhx_gis = json.loads(re.compile('window._sharedData = ({.*?});', re.DOTALL).search(r).group(1))['nonce']
+
+    ppc = re.search(r'ProfilePageContainer.js/(.*?).js', r).group(1)
+    r = requests.get('https://www.instagram.com/static/bundles/es6/ProfilePageContainer.js/' + ppc + '.js').text
+    query_hash = re.findall(r'{value:!0}\);const o=\"(.*?)\"', r)[0]
+
+    global authtokens
+    authtokens = tuple((rhx_gis, query_hash))
+
+
+def const_gis(query):
+    checkTokens()
+    t = authtokens[0] + ':' + query
+    x_instagram_gis = hashlib.md5(t.encode("utf-8")).hexdigest()
+    return x_instagram_gis
+
 
 def usernameToUserId(user):
-    r1 = requests.get('https://www.instagram.com/web/search/topsearch/?query=' + user, headers={
+    r = requests.get('https://www.instagram.com/web/search/topsearch/?query=' + user, headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0'}).text
 
+    if json.loads(r).get("message") == 'rate limited':
+        print(
+            '[x] Rate limit reached!\n[#] Unchecked Username: {}\n[!] Try again in a few minutes.\n'.format(user))
+        exit()
+
     try:
-        if json.loads(r1)['users'][0]['user']['username'] == user:
-            return json.loads(r1)['users'][0]['user']['pk']
-    except IndexError:
+        if json.loads(r)['users'][0]['user']['username'] == user:
+            return json.loads(r)['users'][0]['user']['pk']
+    except:
         return False
 
 
 def useridToUsername(userid):
-    r1 = requests.get('https://instagram.com/instagram/', headers={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0', }).text
-    rhx_gis = json.loads(re.compile('window._sharedData = ({.*?});', re.DOTALL).search(r1).group(1))['nonce']
-
-    ppc = re.search(r'ProfilePageContainer.js/(.*?).js', r1).group(1)
-    r2 = requests.get('https://www.instagram.com/static/bundles/es6/ProfilePageContainer.js/' + ppc + '.js').text
-    query_hash = re.findall(r'{value:!0}\);const o=\"(.*?)\"', r2)[0]
-
+    checkTokens()
     query_variable = '{"user_id":"' + str(userid) + '","include_reel":true}'
-    t = rhx_gis + ':' + query_variable
-    x_instagram_gis = hashlib.md5(t.encode("utf-8")).hexdigest()
-
-    header = {'X-Instagram-GIS': x_instagram_gis,
+    header = {'X-Instagram-GIS': const_gis(query_variable),
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
               'X-Requested-With': 'XMLHttpRequest'}
-    r3 = requests.get(
-        'https://www.instagram.com/graphql/query/?query_hash=' + query_hash + '&variables=' + query_variable,
+    r = requests.get(
+        'https://www.instagram.com/graphql/query/?query_hash=' + authtokens[1] + '&variables=' + query_variable,
         headers=header).text
+    if json.loads(r).get("message") == 'rate limited':
+        print('[x] Rate limit reached!\n[#] Unchecked ID: {}\n[!] Try again in a few minutes..\n'.format(userid))
+        exit()
     try:
-        username = json.loads(r3)['data']['user']['reel']['user']['username']
+        username = json.loads(r)['data']['user']['reel']['user']['username']
         return username
-    except IndexError:
+    except:
         return False
 
 
@@ -55,7 +80,8 @@ def main():
                         help='Set Instagram username', type=str)
     parser.add_argument('-i', '--id', action='store', dest='id',
                         help='Set Instagram userID', type=int)
-    parser.add_argument('-f', '--list', action='store', dest='file', help='Import username/userID per line as .txt file',
+    parser.add_argument('-f', '--list', action='store', dest='file',
+                        help='Import username/userID per line as .txt file',
                         type=str)
     args = parser.parse_args()
 
@@ -69,26 +95,26 @@ def main():
             print('[-] File Not Found :(')
             return 0
 
-        for e in elements:
-            e = e.strip()
-            if e.isdigit():
-                username = useridToUsername(e)
-                if username:
-                    result.append('{}:{}'.format(e, username))
-                else:
-                    print('[-] "{}" Not Found!\n'.format(e))
-            elif e.isalnum():
-                userid = usernameToUserId(e)
-                if userid:
-                    result.append('{}:{}'.format(e, userid))
-                else:
-                    print('[-] "{}" Not Found!\n'.format(e))
-
+        print("Processing...\n")
         with open('result.txt', 'w') as file:
-            for record in result:
-                file.write(record + '\n')
+            for e in elements:
+                e = e.strip()
+                if e.isdigit():
+                    username = useridToUsername(e)
+                    if username:
+                        result.append('{}:{}'.format(e, username))
+                        file.write('{}:{}\n'.format(e, username))
+                    else:
+                        print('[-] "{}" Not Found!\n'.format(e))
+                elif e.isalnum():
+                    userid = usernameToUserId(e)
+                    if userid:
+                        result.append('{}:{}'.format(e, userid))
+                        file.write('{}:{}\n'.format(e, userid))
+                    else:
+                        print('[-] "{}" Not Found!\n'.format(e))
 
-        print('[++] result saved as result.txt')
+        print('[++] Result saved as result.txt')
         return 0
 
     if args.id is not None:
